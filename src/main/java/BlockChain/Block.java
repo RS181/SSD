2a7c3b8628d1c46feedeb6bab7f298a898bba582
RS181 <ruisantos.18181@gmail.com>
2025-02-25 09:59:43 +0000

    Definição inicial de bloco e Exemplo de blockchain

--- src/main/java/BlockChain/Block.java
+++ src/main/java/BlockChain/Block.java
@@ -1,4 +1,49 @@
 package BlockChain;
 
+import java.util.Arrays;
+
 public class Block {
+
+    //TODO: Criar uma classe para transações
+    private  String[] transactions;
+    private int blockHash;
+    private int previousBlockHash;
+
+    private int nonce;
+    private long timestamp;
+
+    public Block(String[] transactions, int previousBlockHash) {
+        this.transactions = transactions;
+        this.previousBlockHash = previousBlockHash;
+
+        //TODO: Ajustar para algoritmo de hash suposto (suponho que seja SHA256)
+        this.blockHash = Arrays.hashCode(new int[] {Arrays.hashCode(transactions),this.previousBlockHash} );
+    }
+
+
+    /* Getter's & Setter's*/
+    public int getBlockHash() {
+        return blockHash;
+    }
+
+    public int getPreviousHash() {
+        return previousBlockHash;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void incrementNonce(){
+        this.nonce++;
+    }
+
+    @Override
+    public String toString() {
+        return  "PreviousBlockHash = " + this.previousBlockHash + ", "+
+                //"Timestamp = " + this.timestamp +  ", "+
+                "BlockHash = " + this.blockHash + ", "+
+                //"Nonce = " + this.nonce  + ", "+
+                "Transactions = " + Arrays.toString(transactions) + "\n";
+    }
 }
---
+++ src/main/java/BlockChain/BlockChainProgram.java
@@ -0,0 +1,39 @@
+package BlockChain;
+
+import java.util.ArrayList;
+
+public class BlockChainProgram {
+
+    public static void main(String[] args) {
+
+        // Demonstrate a series of blocks in a chain
+        ArrayList<Block> blockChain = new ArrayList<>();
+
+        //First Block
+        String[] initialValues = {"Shad has $700", "Miguel has $550"};
+        Block firstBlock = new Block(initialValues,0);
+        blockChain.add(firstBlock);
+
+        System.out.println("first Block is " + firstBlock.toString());
+        System.out.println("The block chain is " + blockChain.toString());
+
+        //Second Block
+        String[] shadGivesItAway = {"Shad gives Tim $40" , "Shad gives Tany $60" ,"Shad gives Terry $100"};
+        Block secondBlock = new Block(shadGivesItAway,firstBlock.getBlockHash());
+        blockChain.add(secondBlock);
+
+        System.out.println("Second Block is " + secondBlock.toString());
+        System.out.println("The block chain is " + blockChain.toString());
+
+
+        //Third Block
+        String[] shadGetsSome = {"Tim gives Shad $10" , "Terry gives $50 to Shad" };
+        Block thirdBlock = new Block(shadGetsSome,secondBlock.getBlockHash());
+        blockChain.add(thirdBlock);
+
+        System.out.println("Third Block is " + thirdBlock.toString());
+        System.out.println("The block chain is " + blockChain.toString());
+
+
+    }
+}
