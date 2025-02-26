555d56076a0a69b35379cc86af07652fcb599f88
RS181 <ruisantos.18181@gmail.com>
2025-02-26 19:35:23 +0000

    Classe Transaction (por preencher)

---
+++ src/main/java/BlockChain/Transaction.java
@@ -0,0 +1,11 @@
+package BlockChain;
+
+/**
+ *
+ * Class that represents a Single transaction between 2 parties in a blockchain
+ */
+public class Transaction {
+    //TODO Qual é o conteudo de uma transação??
+    
+
+}
