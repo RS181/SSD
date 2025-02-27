ab538a0f136274506d95965c7ccf37b7c8dc54b6
RS181 <ruisantos.18181@gmail.com>
2025-02-27 10:12:27 +0000

    Classe para constantes

---
+++ src/main/java/BlockChain/Constants.java
@@ -0,0 +1,19 @@
+package BlockChain;
+
+/**
+ * Class that has some constant's
+ */
+public class Constants {
+
+    /**
+     * Defines the dificulty of mining.
+     * Basicaly defines the number of initial zeros of a valid hash of a block
+     */
+    public static final int DIFFICULTY = 3;
+
+    //TODO: verificar qual o valor apropriado
+    /**
+     * Defines the reward given to a Miner after mining a Block
+     */
+    public static final int MINER_REWARD = 5;
+}
