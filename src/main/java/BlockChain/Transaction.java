86c1eba5e3eca415b0a6df2cdb39e05253939d25
RS181 <ruisantos.18181@gmail.com>
2025-03-07 14:19:09 +0000

    Remoção de assinatura digital e respetivos métodos

--- src/main/java/BlockChain/Transaction.java
+++ src/main/java/BlockChain/Transaction.java
@@ -1,11 +1,75 @@
 package BlockChain;
+import Cryptography.CryptoUtils;
 
-/**
- *
- * Class that represents a Single transaction between 2 parties in a blockchain
- */
+import java.security.*;
+import java.util.Arrays;
+import java.util.Base64;
+
+/*Class that represents a Transaction (Operation) */
 public class Transaction {
-    //TODO Qual é o conteudo de uma transação??
-    
+    public enum TransactionType {
+        CREATE_AUCTION, START_AUCTION, CLOSE_AUCTION, PLACE_BID
+    }
+    private final PublicKey senderPublicKey;
+    private final TransactionType type;
+    private final String transactionId;
+    private final String auctionId;
+    private final long timestamp;
+    private final double bidAmount; // Only used in "PLACE_BID"
 
+
+    public Transaction(PublicKey senderPublicKey, TransactionType type, String auctionId, double bidAmount, long timestamp) {
+        this.senderPublicKey = senderPublicKey;
+        this.type = type;
+        this.auctionId = auctionId;
+        this.bidAmount = bidAmount;
+        this.timestamp = timestamp;
+        this.transactionId = generateTransactionId();
+    }
+
+    /**
+     * generates a tranction Id based on Transaction data
+     * @return genarated Transaction Id
+     */
+    private String generateTransactionId() {
+        return Base64.getEncoder().encodeToString(
+                (senderPublicKey.toString() + type + auctionId + bidAmount + timestamp).getBytes());
+    }
+
+
+    @Override
+    public String toString() {
+        return "\n\tTransaction Details:\n" +
+                "\t\tTransaction Id: " + transactionId + "\n" +
+                "\t\tAuction Id: " + auctionId + "\n" +
+                "\t\tSender: " + senderPublicKey.toString() + "\n" +
+                "\t\tTransaction type: " + type + "\n" +
+                "\t\tBid ammount:  " + bidAmount + "\n" +
+                "\t\tTimestamp: " + Utils.convertTime(timestamp) + "\n"+
+    }
+
+    /*Getter's*/
+    public String getTransactionId() {
+        return transactionId;
+    }
+
+    public PublicKey getSenderPublicKey() {
+        return senderPublicKey;
+    }
+
+    public TransactionType getType() {
+        return type;
+    }
+
+    public String getAuctionId() {
+        return auctionId;
+    }
+
+    public double getBidAmount() {
+        return bidAmount;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
 }
