4d7bdcf2d0356bd267e61be9b268eabbad26c5ad
RS181 <ruisantos.18181@gmail.com>
2025-03-07 18:57:16 +0000

    Ajustes

--- src/main/java/BlockChain/BlockChainProgram.java
+++ src/main/java/BlockChain/BlockChainProgram.java
@@ -1,7 +1,5 @@
 package BlockChain;
 
-import java.util.ArrayList;
-
 public class BlockChainProgram {
 
     public static void main(String[] args) {
@@ -11,30 +9,30 @@ public class BlockChainProgram {
         String[] initialValues = {"Shad has $700", "Miguel has $550"};
 
         //First mined Block
-        Miner m = new Miner(initialValues,"");
-        Block firstBlock = m.getNewestBlock();
+        Miner m = new Miner();
+        Block firstBlock = m.mineBlock(initialValues,"");
         System.out.println("First mined Block is " + firstBlock.toString());
         //System.out.println("The block chain is " + blockChain.toString());
 
         String[] shadGivesItAway = {"Shad gives Tim $40" , "Shad gives Tany $60" ,"Shad gives Terry $100"};
 
         //Second mined Block
-        m = new Miner(shadGivesItAway,firstBlock.getBlockHash());
-        Block secondBlock = m.getNewestBlock();
+        m = new Miner();
+        Block secondBlock = m.mineBlock(shadGivesItAway,firstBlock.getBlockHash());
         System.out.println("Second mined Block is " + secondBlock.toString());
         //System.out.println("The block chain is " + blockChain.toString());
 
         String[] shadGetsSome = {"Tim gives Shad $10" , "Terry gives $50 to Shad" };
 
         //Third mined Block
-        m = new Miner(shadGetsSome,secondBlock.getBlockHash());
-        Block thirdBlock = m.getNewestBlock();
+        m = new Miner();
+        Block thirdBlock = m.mineBlock(shadGetsSome,secondBlock.getBlockHash());
         System.out.println("Third mined Block is " + thirdBlock.toString());
         //System.out.println("The block chain is " + blockChain.toString());
 
-        System.out.println("Adicionar primeiro bloco : " + blockchain.addBlock(firstBlock));
-        System.out.println("Adicionar segundo bloco : " + blockchain.addBlock(secondBlock));
-        System.out.println("Adicionar terceiro bloco : " + blockchain.addBlock(thirdBlock));
+        System.out.println("Adicionar primeiro bloco : " + blockchain.addBlock(firstBlock,m));
+        System.out.println("Adicionar segundo bloco : " + blockchain.addBlock(secondBlock,m));
+        System.out.println("Adicionar terceiro bloco : " + blockchain.addBlock(thirdBlock,m));
 
         System.out.println(blockchain);
         System.out.println(blockchain.checkCurrentChain());
