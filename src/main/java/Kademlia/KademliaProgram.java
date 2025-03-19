edeb0c20c4d5832d901c46049063e5756d6f570d
RS181 <ruisantos.18181@gmail.com>
2025-03-19 21:15:53 +0000

    pequenos ajustes

--- src/main/java/Kademlia/KademliaProgram.java
+++ src/main/java/Kademlia/KademliaProgram.java
@@ -8,11 +8,11 @@ import java.util.Arrays;
 public class KademliaProgram {
 
     public static void main(String[] args) {
-        Node n1 = new Node("localhost",1234);
+        Node n1 = new Node("localhost",1234,true);
 
         System.out.print(n1.getRoutingTable());
 
-        Node n2 = new Node("localhost",4321);
+        Node n2 = new Node("localhost",4321,true);
 
         n1.getRoutingTable().addNodeToBucketList(n2);
         System.out.print(n1.getRoutingTable());
