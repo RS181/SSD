6238c5f3b2fd0422feddadbac0e25d7215e762f7
RS181 <ruisantos.18181@gmail.com>
2025-04-05 11:00:46 +0100

    Método join Network do kademlia (testei e funcionou)

--- src/main/java/Kademlia/Operations.java
+++ src/main/java/Kademlia/Operations.java
@@ -4,6 +4,7 @@ import BlockChain.Block;
 import P2P.PeerComunication;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -29,6 +30,10 @@ public class Operations {
         return false;
     }
 
+    /**
+     * @param senderNode
+     * @param targetNodeId
+     */
     public static void findNode(Node senderNode, String targetNodeId){
         // Contains all nodes received in FIND_NODE response
         List<Node> closestNodesToTarget = new ArrayList<>();
@@ -78,34 +83,39 @@ public class Operations {
 
     // TODO: Join network ainda não esta a funcionar
     public static void joinNetwork(Node joiningNode, Node bootstrap){
-        List<Node> closestNodesToTarget = new ArrayList<>();
-        int newNodes;
-        int i = 0;
-
-        System.out.println("=====JOIN NETWORK Iteration ["+i+"]=====");
-        // First we get the K closest Nodes to joining Node and update
+        System.out.println("=====JOIN NETWORK Iteration [0]=====");
+        // Firstly we get the K closest Nodes to joining Node and update
         // the joining Node's routing table and list of neighbours
+        // Note: We have to guarante tha k is large enough, so that
+        // we receive all nodes the bootstrap nodes knows, so that we
+        // can send ADD_PEER(joiningNode) to all of them (i.e. we
+        // broadcast the joining node to all peer's the bootstrap nodes
+        // knows)
         List<Node> kClosestNodes = (List<Node>) PeerComunication.sendMessageToPeer(
                         bootstrap.getIpAddr(), bootstrap.getPort(), "FIND_NODE", joiningNode.getNodeId());
         updatePeer(joiningNode,kClosestNodes);
 
-        // Add the joining Node to bootstrap's routing table and list of neighbours
+        // Seconly we add the joining Node to bootstrap's routing table and list of neighbours
         PeerComunication.sendMessageToPeer(
                 bootstrap.getIpAddr(),bootstrap.getPort(),"ADD_PEER",joiningNode);
         System.out.println("Closest Nodes to [" +joiningNode +"] =" + kClosestNodes);
         System.out.println("====================================");
 
-        i++;
+        //Finaly broadcast ADD_PEER, to add joining Node to all
+        // the nodes the bootstrap node Knows (in principle all
+        // the nodes that are part of the network)
+        System.out.println("=====JOIN NETWORK Iteration [1]=====");
 
-        // Do the FIND_NODE iteration process until no new nodes are discovered
-        while (true){
-            break;
-        }
+        for( Node n : kClosestNodes)
+            updatePeer(n,new ArrayList<>(Arrays.asList(joiningNode)));
+        System.out.println("====================================");
+
     }
 
     /**
-     * Updates the routing table and list of knownNeighbours of a certain Peer
-     * @param senderNode Node that we are going to send a list of Nodes
+     * Updates the routing table and list of known Neighbours of a certain Peer
+     * @param senderNode Node that we are going to send a list of Nodes to update
+     *                   its routing table and list of known Neighbours
      * @param closestNodesToTarget list of Nodes we are going to send
      */
     private static void updatePeer(Node senderNode, List<Node> closestNodesToTarget) {
