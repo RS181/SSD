d8dfa6d81dc77cc4fb320697f4f72ebc4c538fbf
RS181 <ruisantos.18181@gmail.com>
2025-04-05 10:22:05 +0100

    Observações importantes sobre MAX_RETURN_FIND_NODES

--- src/main/java/Kademlia/Constants.java
+++ src/main/java/Kademlia/Constants.java
@@ -13,14 +13,19 @@ public class Constants {
     /**
      * Defines the number of Nodes returned by FIND operation
      * <p>
-     * IMPORTANT!!!
-     * NOTE: In this case we have to adjust this constant so that
+     * VERY IMPORTANT!!!
+     * NOTE 1 : In this case we have to adjust this constant so that
      * when we receive the nodes of a FIND operation, we receive
      * all nodes that are present in the network, so that all
      * nodes know eachother (a.k.a all nodes are bootstrap nodes).
+     *
+     * NOTE 2 : We have to garante that this values also represents, the
+     * number of nodes that the network can have (Since we are doing
+     * localy, we assume that at max we are going to have 4 to 6 nodes
+     * , but in other cenarios this values need's to be adjusted)
      * </p>
      */
-    public static final int MAX_RETURN_FIND_NODES = 4;
+    public static final int MAX_RETURN_FIND_NODES = 6;
 
     /**
      * Defines the number of Nodes, in the Routing table,
