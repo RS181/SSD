8e069edc9a3a9cb8210d6a2f8242e01785c9db89
RS181 <ruisantos.18181@gmail.com>
2025-04-08 16:08:50 +0100

    Testes manuais a Blockchain

--- src/main/java/BlockChain/BlockChainProgram.java
+++ src/main/java/BlockChain/BlockChainProgram.java
@@ -1,7 +1,6 @@
 package BlockChain;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class BlockChainProgram {
 
@@ -11,14 +10,34 @@ public class BlockChainProgram {
         Miner user1 = new Miner();
         Miner user2 = new Miner();
 
-        ArrayList<Transaction> transactions = new ArrayList<>();
+        ArrayList<Transaction> firstTransactions = new ArrayList<>();
+        firstTransactions.add(new Transaction("user1", Transaction.TransactionType.START_AUCTION, "AUC123", 0, System.currentTimeMillis()));
+        firstTransactions.add(new Transaction("user2", Transaction.TransactionType.PLACE_BID, "AUC123", 100.50, System.currentTimeMillis()));
+        firstTransactions.add(new Transaction("user1", Transaction.TransactionType.PLACE_BID, "AUC123", 150.75, System.currentTimeMillis()));
+        Block firstBlock = user1.mineBlock(firstTransactions,"");
+        System.out.println("Add result = " + blockchain.addBlock(firstBlock,user1.getPublicKey()));
+        System.out.println("Blockchain status = " + blockchain.checkCurrentChain());
+        System.out.println(blockchain.getAvailableAuctions());
 
-        transactions.add(new Transaction("user1", Transaction.TransactionType.CREATE_AUCTION, "AUC123", 0, System.currentTimeMillis()));
-        transactions.add(new Transaction("user2", Transaction.TransactionType.PLACE_BID, "AUC123", 100.50, System.currentTimeMillis()));
-        transactions.add(new Transaction("user1", Transaction.TransactionType.PLACE_BID, "AUC123", 150.75, System.currentTimeMillis()));
-        transactions.add(new Transaction("user2", Transaction.TransactionType.CLOSE_AUCTION, "AUC123", 0, System.currentTimeMillis()));
 
-        Block firstBlock = user1.mineBlock(transactions,"");
+        ArrayList<Transaction> secondTransactions = new ArrayList<>();
+        secondTransactions.add(new Transaction("user1", Transaction.TransactionType.START_AUCTION, "batatas", 0, System.currentTimeMillis()));
+        secondTransactions.add(new Transaction("user1", Transaction.TransactionType.CLOSE_AUCTION, "AUC123", 0, System.currentTimeMillis()));
+
+        Block secondBlock = user2.mineBlock(secondTransactions,blockchain.getLastBlock().getBlockHash());
+        System.out.println("Add result = " + blockchain.addBlock(secondBlock,user2.getPublicKey()));
+        System.out.println("Blockchain status = " + blockchain.checkCurrentChain());
+        System.out.println(blockchain.getAvailableAuctions());
+
+        ArrayList<Transaction> thirdTransactions = new ArrayList<>();
+        thirdTransactions.add(new Transaction("user1", Transaction.TransactionType.START_AUCTION, "AUC123", 0, System.currentTimeMillis()));
+
+        Block thirdBlock = user2.mineBlock(thirdTransactions,blockchain.getLastBlock().getBlockHash());
+        System.out.println("Add result =" +  blockchain.addBlock(thirdBlock, user2.getPublicKey()));
+        System.out.println("Blockchain status = " + blockchain.checkCurrentChain());
+        System.out.println(blockchain.getAvailableAuctions());
+
+
     }
 
 
